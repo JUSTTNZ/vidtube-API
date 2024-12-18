8d0edb703afa03b0786375e8492ee4f4c1389694
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-12-18 04:44:24 -0800

    handled the healthCheck response with postman

--- src/app.js
+++ src/app.js
@@ -13,4 +13,9 @@ app.use(
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+
+// import routes
+import healthCheckRouter from "./routes/healthCheckRoute.js"
+
+app.use("/api/v1/healthCheck", healthCheckRouter)
 export { app }
