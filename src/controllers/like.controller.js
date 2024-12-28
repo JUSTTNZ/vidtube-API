1cc501be5356d3f99826f66391172ee7962ced92
JUSTTNZ <chukwudiconfidenceosinachi@gmail.com>
2024-12-27 22:55:22 -0800

    togglevideolike, toggletweetlike and togglecomment now working.

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,6 +1,8 @@
 import {isValidObjectId} from "mongoose"
 import {Like} from "../models/like.models.js"
 import { Video } from "../models/video.models.js"
+import {Tweet} from "../models/tweet.models.js"
+import { Comment } from "../models/comment.models.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -22,14 +24,11 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         let like = await Like.findOne({video: videoId, likedBy: userId})
         
         if(like) {
+            await Like.findByIdAndDelete(like._id);
             let video = await Video.findByIdAndUpdate(videoId, {
-                $inc: {likes: 0}
+                $inc: {likes: -1}
             }, { new: true })
             console.log("video unlike successfully", like)
-
-            if(!video) {
-                throw new ApiError(404, "Video not found")
-            }
 
             return res
                 .status(200)
@@ -57,24 +56,154 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
                 .json(new ApiResponse(200, { like: populatedLike, totalLikes: video.likes }, "Video liked successfully"))
         }
     } catch (error) {
-        return res.status(500).json(new ApiError(500, "An error occurred while liking the video"))
+        console.log(error);
+        throw new ApiError(400,"An error occurred while liking the video")
     }
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params;
+    
     //TODO: toggle like on comment
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(404, "commentID is invalid");
+    }
 
-})
+    try {
+        const { userId } = req.body;
+    
+        let like = await Like.findOne({ comment: commentId, likedBy: userId });
+
+        if (like) {
+            
+            await Like.findByIdAndDelete(like._id);
+
+            let comment = await Comment.findByIdAndUpdate(
+                commentId,
+                { $inc: { likes: -1 } },
+                { new: true }
+            );
+
+            if (!comment) {
+                throw new ApiError(404, "Comment not found");
+            }
+
+            console.log("Comment unliked successfully", like);
+
+            return res.status(200).json(new ApiResponse(200, { totalLikes: comment.likes }, "Comment unliked successfully"));
+        } else {
+
+            like = new Like({
+                comment: commentId,
+                likedBy: userId
+            });
+
+            await like.save();
+
+            let comment = await Comment.findByIdAndUpdate(
+                commentId,
+                { $inc: { likes: 1 } },
+                { new: true }
+            );
+
+            if (!comment) {
+                throw new ApiError(404, "Comment not found");
+            }
+
+            const populatedLike = await Like.findById(like._id).populate('likedBy', 'username');
+
+            return res.status(200).json(new ApiResponse(200, { like: populatedLike, totalLikes: comment.likes }, "Comment liked successfully"));
+        }
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(400, "An error occurred while liking the comment");
+    }
+});
+
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+
     //TODO: toggle like on tweet
-}
-)
+    const { tweetId } = req.params;
+    
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(404, "tweetID is invalid");
+    }
+
+    try {
+        const { userId } = req.body;
+        
+        let like = await Like.findOne({ tweet: tweetId, likedBy: userId });
+
+        if (like) {
+            await Like.findByIdAndDelete(like._id);
+
+            let tweet = await Tweet.findByIdAndUpdate(
+                tweetId,
+                { $inc: { likes: -1 } },
+                { new: true }
+            );
+
+            if (!tweet) {
+                throw new ApiError(404, "Tweet not found");
+            }
+
+            console.log("Tweet unliked successfully", like);
+
+            return res.status(200).json(new ApiResponse(200, { totalLikes: tweet.likes }, "Tweet unliked successfully"));
+        } else {
+            like = new Like({
+                tweet: tweetId,
+                likedBy: userId
+            });
+
+            await like.save();
+
+            let tweet = await Tweet.findByIdAndUpdate(
+                tweetId,
+                { $inc: { likes: 1 } },
+                { new: true }
+            );
+
+            if (!tweet) {
+                throw new ApiError(404, "Tweet not found");
+            }
+
+            const populatedLike = await Like.findById(like._id).populate('likedBy', 'username');
+
+            return res.status(200).json(new ApiResponse(200, { like: populatedLike, totalLikes: tweet.likes }, "Tweet liked successfully"));
+        }
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(400, "An error occurred while liking the tweet");
+    }
+});
+
+
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
+    const {userId} = req.params
+
+    if(!isValidObjectId(userId)) {
+        throw new ApiError(401, "userID is invalid")
+    }
+
+    try {
+        const {videoId} = req.body
+        let likedVideos = await Like.find({videoIds: videoId, likedBy: userId}).populate([{path: 'likedBy', select: 'username'}, {path: 'video', select: 'title'}])
+
+        if(!likedVideos) {
+            throw new ApiError(400, "user haven't liked any video")
+        }
+        
+        return res
+            .status(200)
+            .json(new ApiResponse(201, likedVideos, "list of liked videos successfully loaded"))
+    } catch (error) {
+        console.log(error);
+        throw new ApiError(400, "cant get user list of liked video")
+    }
 })
 
 export {
